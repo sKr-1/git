a033d7af20b014f67937d029f525f9915428671f
I528666 <sanjeev.kumar.ranjan@sap.com>
2024-03-16 12:41:06 +0530

    random

--- src/main/java/com/x/git/GitApplication.java
+++ src/main/java/com/x/git/GitApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitApplication {
 
 	public static void main(String[] args) {
+		System.out.println("random");
 		SpringApplication.run(GitApplication.class, args);
 	}
 
